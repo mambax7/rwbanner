4e3fd1f58f4fb31b34fcef21526bde0d48d6ce7e
mambax7 <mambax7@gmail.com>
2021-03-10 05:55:56 -0500

    check upload folders

--- config/config.php
+++ config/config.php
@@ -34,14 +34,14 @@ return (object)[
     ],
     'uploadFolders'  => [
         XOOPS_UPLOAD_PATH . '/' . $moduleDirName,
-        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/category',
-        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/screenshots',
+//        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/category',
+//        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/screenshots',
         //XOOPS_UPLOAD_PATH . '/flags'
     ],
     'copyBlankFiles' => [
         XOOPS_UPLOAD_PATH . '/' . $moduleDirName,
-        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/category',
-        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/screenshots',
+//        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/category',
+//        XOOPS_UPLOAD_PATH . '/' . $moduleDirName . '/screenshots',
         //XOOPS_UPLOAD_PATH . '/flags'
     ],
 
